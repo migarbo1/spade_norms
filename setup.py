787aa775b19ebb6ffca749cad3f30a4b2f5132dc
jpalanca <jpalanca@dsic.upv.es>
2023-05-16 10:41:29 +0200

    Parse requirements files

--- setup.py
+++ setup.py
@@ -10,23 +10,34 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
 
-test_requirements = ['pytest>=3', ]
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    with open(filename) as f:
+        lineiter = [line.strip() for line in f]
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+requirements = parse_requirements("requirements.txt")
+
+test_requirements = parse_requirements("requirements_dev.txt")
 
 setup(
     author="Javi Palanca",
     author_email='jpalanca@dsic.upv.es',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Internet :: XMPP',
     ],
     description="SPADE Normative Plugin",
     install_requires=requirements,
