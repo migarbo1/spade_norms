f9197734ff75a32b739d4ebafd6667f74966bec0
miguel <miguel.garciabohigues@hotmail.com>
2023-05-19 11:58:12 +0200

    debug lines deleted

--- spade_norms/engines/reasoning_engine.py
+++ spade_norms/engines/reasoning_engine.py
@@ -6,23 +6,19 @@ class NormativeReasoningEngine():
     def __init__(self):
         pass
 
-    def inference(norm_response: NormativeResponse):
+    def inference(self, norm_response: NormativeResponse):
         '''
         This function allows the agent to reason about whether to perform an action or not.
         You can override this method to change this behaviour.
         '''
-        if norm_response.responseType == NormativeActionStatus.NOT_REGULATED or NormativeActionStatus.ALLOWED:
-            print('Doing action because is not regulated or allowed')
+        if norm_response.responseType == NormativeActionStatus.NOT_REGULATED or norm_response.responseType == NormativeActionStatus.ALLOWED:
             return True
         
         if norm_response.responseType == NormativeActionStatus.INVIOLABLE:
-            print('Not doing action because is inviolable')
             return False
         
         if norm_response.responseType == NormativeActionStatus.MIXED:
-            print('Mixed responses, doing action only if benefit is greater than penalty')
             return norm_response.total_reward > norm_response.total_penalty
 
         if norm_response.responseType == NormativeActionStatus.FORBIDDEN:
-            print('Forbiden action, doing only 1 out of 10 times')
             return random.random() > 0.9
