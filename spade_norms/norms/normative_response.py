37a93c188a8c8fb4bcf0db539bd3a2295c0fc528
miguel <miguel.garciabohigues@hotmail.com>
2023-05-19 11:57:40 +0200

    fixed bug in or operand and lists not restarting

--- spade_norms/norms/normative_response.py
+++ spade_norms/norms/normative_response.py
@@ -2,11 +2,11 @@ from .norm_enums import NormativeActionStatus
 from .norm import Norm
 
 class NormativeResponse():
-    def __init__(self, responseType: NormativeActionStatus = None, norms_allowing: list = [], norms_forbidding : list = []
+    def __init__(self, responseType: NormativeActionStatus = None, norms_allowing: list = None, norms_forbidding : list = None
                 , total_reward:float = 0.0, total_penalty: float = 0.0):
         self.responseType = responseType
-        self.norms_allowing = norms_allowing
-        self.norms_forbidding = norms_forbidding
+        self.norms_allowing = norms_allowing if norms_allowing != None else [] 
+        self.norms_forbidding = norms_forbidding if norms_forbidding != None else [] 
         self.total_reward = total_reward
         self.total_penalty = total_penalty
 
@@ -20,10 +20,10 @@ class NormativeResponse():
         self.norms_allowing.append(norm)
         self.total_reward += norm.reward
 
-        if self.responseType == None or NormativeActionStatus.ALLOWED or NormativeActionStatus.NOT_REGULATED:
+        if self.responseType == None or self.responseType == NormativeActionStatus.ALLOWED or self.responseType == NormativeActionStatus.NOT_REGULATED:
             self.responseType = NormativeActionStatus.ALLOWED
 
-        elif self.responseType == NormativeActionStatus.MIXED or NormativeActionStatus.FORBIDDEN:
+        elif self.responseType == NormativeActionStatus.MIXED or self.responseType == NormativeActionStatus.FORBIDDEN:
             self.responseType = NormativeActionStatus.MIXED
         else:
             self.responseType = NormativeActionStatus.INVIOLABLE
@@ -35,14 +35,17 @@ class NormativeResponse():
         - if there has been at least one norm allowed, then status will be MIXED
         - if there has been a forbidden state for an inviolable norm, status will remain the same.
         '''
-        self.norms_allowing.append(norm)
+        self.norms_forbidding.append(norm)
         self.total_penalty += norm.penalty
 
         if norm.inviolable:
             self.responseType = NormativeActionStatus.INVIOLABLE
 
-        if self.responseType == None or NormativeActionStatus.FORBIDDEN or NormativeActionStatus.NOT_REGULATED:
+        if self.responseType == None or self.responseType == NormativeActionStatus.FORBIDDEN or self.responseType == NormativeActionStatus.NOT_REGULATED:
             self.responseType = NormativeActionStatus.FORBIDDEN
             
-        elif self.responseType == NormativeActionStatus.MIXED or NormativeActionStatus.ALLOWED:
+        elif self.responseType == NormativeActionStatus.MIXED or self.responseType == NormativeActionStatus.ALLOWED:
             self.responseType = NormativeActionStatus.MIXED
+
+    def __str__(self):
+        return '{' +  '\tresponse type: {},\n\tnorms_allow: {},\n\tnorms_forbidding: {},\n\ttotal_reward: {},\n\ttotal_penalty: {}'.format(self.responseType, self.norms_allowing, self.norms_forbidding, self.total_reward, self.total_penalty)  + '}'
