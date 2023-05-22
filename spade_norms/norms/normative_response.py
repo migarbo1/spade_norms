fdfcc197a6c64345060f9bbf4b7aca20e9b8e68c
miguel <miguel.garciabohigues@hotmail.com>
2023-05-22 15:56:38 +0200

    change in naming of norms that you are following and breaking

--- spade_norms/norms/normative_response.py
+++ spade_norms/norms/normative_response.py
@@ -3,13 +3,12 @@ from .norm import Norm
 from ..actions.normative_action import NormativeAction
 
 class NormativeResponse():
-    def __init__(self, action: NormativeAction = None, responseType: NormativeActionStatus = None, norms_allowing: list = None, norms_forbidding : list = None
+    def __init__(self, action: NormativeAction = None, responseType: NormativeActionStatus = None, norms_following: list = None, norms_breaking : list = None
                 , total_reward:float = 0.0, total_penalty: float = 0.0):
         self.action = action
         self.responseType = responseType
-        # normas que cumples o no cumples -> cambiar naming
-        self.norms_allowing = norms_allowing if norms_allowing != None else [] 
-        self.norms_forbidding = norms_forbidding if norms_forbidding != None else [] 
+        self.norms_following = norms_following if norms_following != None else [] 
+        self.norms_breaking = norms_breaking if norms_breaking != None else [] 
         self.total_reward = total_reward
         self.total_penalty = total_penalty
 
@@ -20,7 +19,7 @@ class NormativeResponse():
         - if there has been at least one norm violated, then status will be MIXED
         - if there has been a forbidden state for an INVIOLABLE norm, status will remain the same.
         '''
-        self.norms_allowing.append(norm)
+        self.norms_following.append(norm)
         self.total_reward += norm.reward
 
         if self.responseType == None or self.responseType == NormativeActionStatus.ALLOWED or self.responseType == NormativeActionStatus.NOT_REGULATED:
@@ -38,7 +37,7 @@ class NormativeResponse():
         - if there has been at least one norm allowed, then status will be MIXED
         - if there has been a forbidden state for an inviolable norm, status will remain the same.
         '''
-        self.norms_forbidding.append(norm)
+        self.norms_breaking.append(norm)
         self.total_penalty += norm.penalty
 
         if norm.inviolable:
@@ -51,4 +50,4 @@ class NormativeResponse():
             self.responseType = NormativeActionStatus.MIXED
 
     def __str__(self):
-        return '{' +  '\tresponse type: {},\n\tnorms_allow: {},\n\tnorms_forbidding: {},\n\ttotal_reward: {},\n\ttotal_penalty: {}'.format(self.responseType, self.norms_allowing, self.norms_forbidding, self.total_reward, self.total_penalty)  + '}'
+        return '{' +  '\tresponse type: {},\n\norms_complying: {},\n\norms_breaking: {},\n\ttotal_reward: {},\n\ttotal_penalty: {}'.format(self.responseType, self.norms_following, self.norms_breaking, self.total_reward, self.total_penalty)  + '}'
