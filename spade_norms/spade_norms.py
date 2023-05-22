ac6ca19f03eb282cf782a2fd7b7ac34715687aa4
miguel <miguel.garciabohigues@hotmail.com>
2023-05-22 16:06:37 +0200

    added print message to notify user that action has been skiped.

--- spade_norms/spade_norms.py
+++ spade_norms/spade_norms.py
@@ -13,50 +13,54 @@ class NormativeMixin:
         self.normative = NormativeComponent(self)
 
 class NormativeComponent:
-    def __init__(self, agent: Agent, normative_engine: NormativeEngine = None):
-        #add reasoning engine as parameter
+    def __init__(self, agent: Agent, normative_engine: NormativeEngine = None, reasoning_engine: NormativeReasoningEngine = None):
         self.actions = {}
         self.agent = agent
         self.normative_engine = normative_engine
-        self.reasoning_engine = NormativeReasoningEngine()
+        self.reasoning_engine = NormativeReasoningEngine() if reasoning_engine == None else reasoning_engine
 
     def set_normative_engine(self, normative_engine: NormativeEngine):
         self.normative_engine = normative_engine
 
     def perform(self, action_name: str, *args, **kwargs):
-        # informat usuario si no se ha ejecutado la acción
         self.__check_exists(action_name)
-        try:
-            action = self.actions[action_name]
-            if self.normative_engine != None:
-                normative_response = self.normative_engine.check_legislation(action, self.agent)
-                do_action = self.reasoning_engine.inference(normative_response)
-            else:
-                do_action = True
-            if do_action:
-                action_result = self.actions[action_name].action_fn(self.agent, *args,**kwargs)
+        action = self.actions[action_name]
+        if self.normative_engine != None:
+            normative_response = self.normative_engine.check_legislation(action, self.agent)
+            do_action = self.reasoning_engine.inference(normative_response)
+        else:
+            do_action = True
+        if do_action:
+            try:
+                action_result = self.actions[action_name].action_fn(self.agent, *args, **kwargs)
                 if action_result != None:
                     return action_result
-        except Exception:
-            logging.error(traceback.format_exc())
-            print("Error performing action: ", sys.exc_info()[0])
+            except Exception:
+                logging.error(traceback.format_exc())
+                print("Error performing action: ", sys.exc_info()[0])
+        else:
+            #TODO: proceeding for actions not performed
+            print("Action {} not performed due to normative constrictions".format(action_name))
 
     async def performAsync(self, action_name: str, *args, **kwargs):
         self.__check_exists(action_name)
-        try:
-            action = self.actions[action_name]
-            if self.normative_engine != None:
-                normative_response = self.normative_engine.check_legislation(action, self.agent)
-                do_action = self.reasoning_engine.inference(normative_response)
-            else:
-                do_action = True
-            if do_action:
+        action = self.actions[action_name]
+        if self.normative_engine != None:
+            normative_response = self.normative_engine.check_legislation(action, self.agent)
+            do_action = self.reasoning_engine.inference(normative_response)
+        else:
+            do_action = True
+        if do_action:
+            try:    
                 action_result = await self.actions[action_name].action_fn(self.agent, *args, **kwargs)
                 if action_result != None:
                     return action_result
-        except Exception:
-            logging.error(traceback.format_exc())
-            print("Error performing action: ", sys.exc_info()[0])
+            except Exception:
+                logging.error(traceback.format_exc())
+                print("Error performing action: ", sys.exc_info()[0])
+        else:
+            #TODO: proceeding for actions not performed
+            print("Action {} not performed due to normative constrictions".format(action_name))
 
     def __check_exists(self, action_name: str):
         if self.actions.get(action_name, None) == None:
