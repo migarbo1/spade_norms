14e41073804b1ae874abc3e683ec8c94c0e02468
miguel <miguel.garciabohigues@hotmail.com>
2023-06-08 13:01:14 +0200

    normative evalutation logic moved to shared method

--- spade_norms/spade_norms.py
+++ spade_norms/spade_norms.py
@@ -38,12 +38,7 @@ class NormativeComponent:
 
     def perform(self, action_name: str, *args, **kwargs):
         self.__check_exists(action_name)
-        action = self.actions[action_name]
-        if self.normative_engine != None:
-            normative_response = self.normative_engine.check_legislation(action, self.agent)
-            do_action = self.reasoning_engine.inference(normative_response)
-        else:
-            do_action = True
+        do_action = self.__normative_eval(action_name)
         if do_action:
             try:
                 action_result = self.actions[action_name].action_fn(self.agent, *args, **kwargs)
@@ -58,12 +53,7 @@ class NormativeComponent:
 
     async def performAsync(self, action_name: str, *args, **kwargs):
         self.__check_exists(action_name)
-        action = self.actions[action_name]
-        if self.normative_engine != None:
-            normative_response = self.normative_engine.check_legislation(action, self.agent)
-            do_action = self.reasoning_engine.inference(normative_response)
-        else:
-            do_action = True
+        do_action = self.__normative_eval(action_name)
         if do_action:
             try:    
                 action_result = await self.actions[action_name].action_fn(self.agent, *args, **kwargs)
@@ -80,6 +70,15 @@ class NormativeComponent:
         if self.actions.get(action_name, None) == None:
             raise Exception('Action with name {} does not exist in action dict'.format(action_name))
         
+    def __normative_eval(self, action_name):
+        action = self.actions[action_name]
+        if self.normative_engine != None:
+            normative_response = self.normative_engine.check_legislation(action, self.agent)
+            do_action = self.reasoning_engine.inference(normative_response)
+        else:
+            do_action = True
+        return do_action
+
     def add_action(self, action: NormativeAction):
         self.actions[action.name] = action
     
