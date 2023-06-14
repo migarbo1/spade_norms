e17f29ae4fc89018da0f1d56d0c083361af56735
miguel <miguel.garciabohigues@hotmail.com>
2023-06-14 10:42:09 +0200

    normative action default domain set to 0 instead of None

--- spade_norms/actions/normative_action.py
+++ spade_norms/actions/normative_action.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Callable
 
 class NormativeAction():
-    def __init__(self, name: str, action_fn: Callable, domain: Enum = None):
+    def __init__(self, name: str, action_fn: Callable, domain: Enum = 0):
         self.name = name
         self.action_fn = action_fn
         self.domain = domain
